95a11d9f4970a5ec8d9c18409cee93607672780d
Robbepop <robbepop@web.de>
2017-06-19 01:29:54 +0200

    added crate-level description to bitvec sub-crate

--- bitvec/src/lib.rs
+++ bitvec/src/lib.rs
@@ -1,3 +1,27 @@
+//! This library provides an implementation for bitvectors in the context of SMT solving.
+//!
+//! Bitvectors in the context of SMT solving are fixed-sized signed or unsigned integer representatives.
+//! They are needed to express and propagate constant values within an SMT procedure and are required
+//! to be very fast and have low memory overhead since their quantity on a common run might be very high.
+//!
+//! Bitvectors in the context of SMT solving must not be confused with bitvectors that act as storage
+//! for bits as in C++'s `<bitset>`. Although they also allow to operate on them via the typical bitset-based
+//! methods as `intersect`, `union` and `difference`.
+//!
+//! This data structure was designed to be especially fast for smaller instances.
+//! As of now small instances are instances that represent numbers that fit into 64 bits of memory.
+//!
+//! Since bitvector instances might store a variadic and potentially large amount of data they might
+//! require heap allocation and thus are no `Copy` types.
+//! This also eliminates the possibilities to overload operators on them in the current Rust design.
+//!
+//! Instances of bitvectors cannot grow or shrink as normal vectors and are fixed upon construction.
+//! This allows further optimizations and even less memory overhead since no capacity field is required.
+//! 
+//! There are some methods that do not result in a new bitvector instance but that mutate the given
+//! instance and most often are suffixed with `_assign`.
+//!
+
 #![feature(untagged_unions)]
 #![feature(unique)]
 
@@ -56,7 +80,7 @@ impl From<Bits> for u32 {
 const INLINE_BITS  : u32 = 64;
 
 const UNDEF_MARKER : u32 = 0;
-const POISON_MARKER: u32 = 0xFFFF_FFFF;
+const POISON_MARKER: u32 = 0xA000_0000;
 
 pub struct BitVec {
 	/// Number of bits used by this bitvector.
