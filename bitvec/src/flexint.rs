1ea4e1687955405ce518eda87cc56f2099b0d9b0
Robbepop <robbepop@web.de>
2017-06-23 02:45:51 +0200

    added some more docs to FlexInt

--- bitvec/src/flexint.rs
+++ bitvec/src/flexint.rs
@@ -192,7 +192,7 @@ impl Hash for Block {
 }
 
 //  =======================================================================
-///  Constructors for `FlexInt`.
+///  Constructors
 /// =======================================================================
 impl FlexInt {
 	/// Creates a new `FlexInt` from a given `bool` value with a bit-width of 1.
@@ -315,6 +315,15 @@ impl FlexInt {
 
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum TargetBitWidth {
+	/// Tells methods to try to infer the resulting bit-width from the input.
+	Infer,
+	/// Tells methods to use the given fixed bit-width as resulting bit-width.
+	Fixed(u32)
+}
+// use self::TargetBitWidth::*;
+
 //  =======================================================================
 ///  Deserialization
 /// =======================================================================
@@ -322,42 +331,106 @@ impl FlexInt {
 
 	/// Creates a new bitvector from the given binary string representation.
 	/// 
+	/// Using the first parameter `bitwidth` the user can either let the method infer the resulting bit-width
+	/// or set it explicitely.
+	/// 
 	/// The format of the binary string must follow some rules.
-	///  - The only allowed characters are ascii `0`, `1` and a digit separator `_`.
-	///  - The input string must contain at least a single `0` or `1` character.
+	///  - The only allowed characters are digits `'0'`, `'1'` and the digit-separator `'_'` (underscore).
+	///  - The input string must contain at least a single `'0'` or `'1'` character.
 	/// 
 	/// In any other case the implementation will return an error.
 	/// 
 	/// **Good Examples**  
 	/// 
-	/// - `"0101"`  
-	/// - `"0111_0010_0000_1110"`  
-	/// - `"11__00"`  
-	/// - `"__0__"`  
+	/// - `"0101"`
+	/// - `"0111_0010_0000_1110"`
+	/// - `"11__00"`
+	/// - `"__0__"`
 	/// 
 	/// **Bad Examples**  
 	/// 
-	/// - `"0102"`  
-	/// - `"01'0001"`  
-	/// - `"foo"`  
-	/// - `"-1001"`  
+	/// - `"0102"`
+	/// - `"01'0001"`
+	/// - `"foo"`
+	/// - `"-1001"`
 	/// 
-	/// Note: The most significant bit (MSB) is on the left.
-	pub fn from_bin_str(binary_str: &str) -> Result<FlexInt> {
+	/// Note:
+	/// 
+	/// - The most significant bit (MSB) is on the left.
+	/// - The bitwidth of the resulting `FlexInt` is infered from the number of valid digits in the input.
+	/// - Input may start with `'0'` which may influence the bit-width of the resulting `FlexInt`.
+	pub fn from_bin_str(bitwidth: TargetBitWidth, binary_str: &str) -> Result<FlexInt> {
 		unimplemented!();
 	}
 
 	/// Creates a new bitvector from the given decimal string representation.
 	/// 
-	/// Note: The most significant bit (MSB) is on the left.
-	pub fn from_dec_str(dec_str: &str) -> Result<FlexInt> {
+	/// Using the first parameter `bitwidth` the user can either let the method infer the resulting bit-width
+	/// or set it explicitely.
+	/// 
+	/// The format of the decimal string must follow some rules.
+	///  - The only allowed characters are digits `'0'`, `'1'`,..,`'9'` and the digit-separator `'_'` (underscore).
+	///  - The input string must contain at least a single valid digit character.
+	/// 
+	/// In any other case the implementation will return an error.
+	/// 
+	/// **Good Examples**  
+	/// 
+	/// - `"3497"`
+	/// - `"0323_0321_9876_5432"`
+	/// - `"85__132"`
+	/// - `"__9__"`
+	/// - `"000075"`
+	/// 
+	/// **Bad Examples**  
+	/// 
+	/// - `"0A5C"`
+	/// - `"13'8273"`
+	/// - `"bar"`
+	/// - `"-1337"`
+	/// 
+	/// Note:
+	/// 
+	/// - The most significant digit is on the left.
+	/// - The bitwidth of the resulting `FlexInt` is infered from the number of valid digits in the input.
+	/// - Input may start with `'0'` which may influence the bit-width of the resulting `FlexInt`.
+	pub fn from_dec_str(bitwidth: TargetBitWidth, dec_str: &str) -> Result<FlexInt> {
 		unimplemented!();
 	}
 
 	/// Creates a new bitvector from the given hexa-decimal string representation.
 	/// 
-	/// Note: The most significant bit (MSB) is on the left.
-	pub fn from_hex_str(hex_str: &str) -> Result<FlexInt> {
+	/// Using the first parameter `bitwidth` the user can either let the method infer the resulting bit-width
+	/// or set it explicitely.
+	/// 
+	/// The format of the decimal string must follow some rules.
+	///  - The only allowed characters are the digits `'0'`, `'1'`,..,`'9'` the alphas `'A'`,`'B'`,..,`'F'` and the digit-separator `'_'` (underscore).
+	///  - The input string must contain at least a single valid alpha-numeric character.
+	/// 
+	/// In any other case the implementation will return an error.
+	/// 
+	/// **Good Examples**  
+	/// 
+	/// - `"AC08"`
+	/// - `"03B8_A30D_EEE2_007"`
+	/// - `"FF__A00"`
+	/// - `"__E__"`
+	/// - `"B008CE"`
+	/// 
+	/// **Bad Examples**  
+	/// 
+	/// - `"ffcc0"`: no small letters!
+	/// - `"0K5X"`
+	/// - `"13'8273"`
+	/// - `"foobar"`
+	/// - `"-MCLVII"`
+	/// 
+	/// Note:
+	/// 
+	/// - The most significant quad is on the left.
+	/// - The bitwidth of the resulting `FlexInt` is infered from the number of valid digits in the input.
+	/// - Input may start with `'0'` which may influence the bit-width of the resulting `FlexInt`.
+	pub fn from_hex_str(bitwidth: TargetBitWidth, hex_str: &str) -> Result<FlexInt> {
 		unimplemented!();
 	}
 
@@ -429,21 +502,15 @@ impl FlexInt {
 		}
 	}
 
-	/// Returns true if all bits of this `FlexInt` are set.
+	/// Returns true if all bits of this `FlexInt` are `1` (one).
 	#[inline]
-	pub fn all(&self) -> bool {
+	pub fn is_ones(&self) -> bool {
 		unimplemented!()
 	}
 
-	/// Returns true if no bits of this `FlexInt` are set.
+	/// Returns true if no bits of this `FlexInt` are `0` (zero).
 	#[inline]
-	pub fn none(&self) -> bool {
-		unimplemented!()
-	}
-
-	/// Returns true if any bit of this `FlexInt` are set.
-	#[inline]
-	pub fn any(&self) -> bool {
+	pub fn is_zeroes(&self) -> bool {
 		unimplemented!()
 	}
 
