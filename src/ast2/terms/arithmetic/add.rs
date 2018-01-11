b0d95638da85f8489b3b08a0ede237e04db848b1
Hero <robbepop@web.de>
2018-01-11 01:07:15 +0100

    format(Formulas): use rust-fmt to auto-format Add and Mul

--- src/ast2/terms/arithmetic/add.rs
+++ src/ast2/terms/arithmetic/add.rs
@@ -4,45 +4,52 @@ use ast2::terms::checks;
 
 /// Re-exports all commonly used items of this module.
 pub mod prelude {
-    pub use super::{
-        Add
-    };
+    pub use super::Add;
 }
 
 /// Add term expression for adding a number of bitvector expressions.
-/// 
+///
 /// Arithmetically sums up all child term expressions.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Add {
     /// The child bitvector expressions.
     pub childs: Vec<Expr>,
     /// The bit width of this expression.
-    /// 
+    ///
     /// All child expressions must respect this bit width.
     /// This is also used to verify integrity of the bit width.
-    pub width: BitWidth
+    pub width: BitWidth,
 }
 
 impl Add {
     /// Creates a new bitvector Add term expression for all of the child
     /// expressions yielded by the given iterator and with the given bit width.
-    /// 
+    ///
     /// # Errors
-    /// 
+    ///
     /// - If the given iterator yields less than two child expressions.
     /// - If not all yielded child expressions are of bitvec type with
     ///   the required bit width.
     pub fn new<I>(width: BitWidth, childs: I) -> Result<Add, String>
-        where I: IntoIterator<Item = Expr>
+    where
+        I: IntoIterator<Item = Expr>,
     {
         let childs = childs.into_iter().collect::<Vec<_>>();
         if childs.len() < 2 {
-            return Err("Requires at least two child expressions to create an Add term expression.".into())
+            return Err(
+                "Requires at least two child expressions to create an Add term expression.".into(),
+            );
         }
-        if childs.iter().any(|c| checks::expect_bitvec_ty_and_width(c, width).is_err()) {
-            return Err("Requires all child expressions to be of bitvec type with the expected bit width.".into())
+        if childs
+            .iter()
+            .any(|c| checks::expect_bitvec_ty_and_width(c, width).is_err())
+        {
+            return Err(
+                "Requires all child expressions to be of bitvec type with the expected bit width."
+                    .into(),
+            );
         }
-        Ok(Add{width, childs})
+        Ok(Add { width, childs })
     }
 }
 
--- src/ast2/terms/arithmetic/mul.rs
+++ src/ast2/terms/arithmetic/mul.rs
@@ -3,43 +3,50 @@ use ast2::terms::checks;
 
 /// Re-exports all commonly used items of this module.
 pub mod prelude {
-    pub use super::{
-        Mul
-    };
+    pub use super::Mul;
 }
 
 /// N-ary Mul term expression.
-/// 
+///
 /// Arithmetically multiplies all child term expressions.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Mul {
     /// The child formula expressions.
     pub childs: Vec<Expr>,
     /// The bit width of this expression.
-    /// 
+    ///
     /// All child expressions must respect this bit width.
     /// This is also used to verify integrity of the bit width.
-    pub width: BitWidth
+    pub width: BitWidth,
 }
 
 impl Mul {
     /// Creates a new `Mul` formula expression.
-    /// 
+    ///
     /// # Errors
-    /// 
+    ///
     /// - If the given iterator has less than two elements.
     /// - If not all expressions yielded by the given iteration are of boolean type.
     pub fn new<I>(width: BitWidth, childs: I) -> Result<Mul, String>
-        where I: IntoIterator<Item = Expr>
+    where
+        I: IntoIterator<Item = Expr>,
     {
         let childs = childs.into_iter().collect::<Vec<_>>();
         if childs.len() < 2 {
-            return Err("Requires at least two child expressions to create an Mul term expression.".into())
+            return Err(
+                "Requires at least two child expressions to create an Mul term expression.".into(),
+            );
         }
-        if childs.iter().any(|c| checks::expect_bitvec_ty_and_width(c, width).is_err()) {
-            return Err("Requires all child expressions to be of bitvec type with the expected bit width.".into())
+        if childs
+            .iter()
+            .any(|c| checks::expect_bitvec_ty_and_width(c, width).is_err())
+        {
+            return Err(
+                "Requires all child expressions to be of bitvec type with the expected bit width."
+                    .into(),
+            );
         }
-        Ok(Mul{width, childs})
+        Ok(Mul { width, childs })
     }
 }
 
