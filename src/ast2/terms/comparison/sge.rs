fde4a18d15eb207fb266cdb7564a831278f8ef48
Hero <robbepop@web.de>
2018-01-13 15:52:52 +0100

    format(SignedGreaterEquals): use rust-fmt to auto format single file

--- src/ast2/terms/comparison/sge.rs
+++ src/ast2/terms/comparison/sge.rs
@@ -2,39 +2,44 @@ use ast2::prelude::*;
 use ast2::terms::checks;
 
 pub mod prelude {
-    pub use super::{
-        SignedGreaterEquals
-    };
+    pub use super::SignedGreaterEquals;
 }
 
 /// Binary signed greater-than-or-equals term expression.
-/// 
+///
 /// # Note
-/// 
+///
 /// Greater equals comparison is different for signed and unsigned parameters.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct SignedGreaterEquals {
     /// The two child term expressions.
     pub childs: P<BinExprChilds>,
     /// The bit width of this expression.
-    /// 
+    ///
     /// All child expressions must respect this bit width.
     /// This is also used to verify integrity of the bit width.
-    pub childs_bitvec_ty: BitvecTy
+    pub childs_bitvec_ty: BitvecTy,
 }
 
 impl SignedGreaterEquals {
     /// Returns a new `SignedGreaterEquals` term expression with the
     /// given child term expressions.
-    /// 
+    ///
     /// # Errors
-    /// 
+    ///
     /// - If any of the two given child expressions is not of bitvec type or
     ///   has an unmatching bit width to the given bit width.
-    pub fn new(bitvec_ty: BitvecTy, lhs: AnyExpr, rhs: AnyExpr) -> Result<SignedGreaterEquals, String> {
+    pub fn new(
+        bitvec_ty: BitvecTy,
+        lhs: AnyExpr,
+        rhs: AnyExpr,
+    ) -> Result<SignedGreaterEquals, String> {
         checks::expect_concrete_bitvec_ty(&lhs, bitvec_ty)?;
         checks::expect_concrete_bitvec_ty(&rhs, bitvec_ty)?;
-        Ok(SignedGreaterEquals{ childs_bitvec_ty: bitvec_ty, childs: BinExprChilds::new_boxed(lhs, rhs) })
+        Ok(SignedGreaterEquals {
+            childs_bitvec_ty: bitvec_ty,
+            childs: BinExprChilds::new_boxed(lhs, rhs),
+        })
     }
 }
 
