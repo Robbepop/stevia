e0adc75d8a6589de6ae716a9155df87396cb2549
Hero <robbepop@web.de>
2018-01-15 00:26:42 +0100

    feat(Simplifier): implement the rest of the expression to support in the new simplifier

--- src/simplifier/base.rs
+++ src/simplifier/base.rs
@@ -89,6 +89,146 @@ pub trait Transformer: Copy {
     fn transform_not(self, not: expr::Not) -> AnyExprAndTransformResult {
         AnyExprAndTransformResult::identity(not)
     }
+
+    fn transform_xor(self, xor: expr::Xor) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(xor)
+    }
+
+    fn transform_implies(self, implies: expr::Implies) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(implies)
+    }
+
+    fn transform_array_equals(self, array_equals: expr::ArrayEquals) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(array_equals)
+    }
+
+    fn transform_array_read(self, array_read: expr::ArrayRead) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(array_read)
+    }
+
+    fn transform_array_write(self, array_write: expr::ArrayWrite) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(array_write)
+    }
+
+    fn transform_bitvec_const(self, bitvec_const: expr::BitvecConst) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(bitvec_const)
+    }
+
+    fn transform_add(self, add: expr::Add) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(add)
+    }
+
+    fn transform_mul(self, mul: expr::Mul) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(mul)
+    }
+
+    fn transform_neg(self, neg: expr::Neg) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(neg)
+    }
+
+    fn transform_sdiv(self, sdiv: expr::SignedDiv) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(sdiv)
+    }
+
+    fn transform_smod(self, smod: expr::SignedModulo) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(smod)
+    }
+
+    fn transform_srem(self, srem: expr::SignedRemainder) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(srem)
+    }
+
+    fn transform_sub(self, sub: expr::Sub) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(sub)
+    }
+
+    fn transform_udiv(self, udiv: expr::UnsignedDiv) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(udiv)
+    }
+
+    fn transform_urem(self, urem: expr::UnsignedRemainder) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(urem)
+    }
+
+    fn transform_bitand(self, bitand: expr::BitAnd) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(bitand)
+    }
+
+    fn transform_bitnot(self, bitnot: expr::BitNot) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(bitnot)
+    }
+
+    fn transform_bitor(self, bitor: expr::BitOr) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(bitor)
+    }
+
+    fn transform_bitxor(self, bitxor: expr::BitXor) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(bitxor)
+    }
+
+    fn transform_concat(self, concat: expr::Concat) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(concat)
+    }
+
+    fn transform_extract(self, extract: expr::Extract) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(extract)
+    }
+
+    fn transform_sext(self, sext: expr::SignExtend) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(sext)
+    }
+
+    fn transform_zext(self, zext: expr::ZeroExtend) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(zext)
+    }
+
+    fn transform_bitvec_equals(self, bitvec_equals: expr::BitvecEquals) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(bitvec_equals)
+    }
+
+    fn transform_sge(self, sge: expr::SignedGreaterEquals) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(sge)
+    }
+
+    fn transform_sgt(self, sgt: expr::SignedGreaterThan) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(sgt)
+    }
+
+    fn transform_sle(self, sle: expr::SignedLessEquals) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(sle)
+    }
+
+    fn transform_slt(self, slt: expr::SignedLessThan) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(slt)
+    }
+
+    fn transform_uge(self, uge: expr::UnsignedGreaterEquals) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(uge)
+    }
+
+    fn transform_ugt(self, ugt: expr::UnsignedGreaterThan) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(ugt)
+    }
+
+    fn transform_ule(self, ule: expr::UnsignedLessEquals) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(ule)
+    }
+
+    fn transform_ult(self, ult: expr::UnsignedLessThan) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(ult)
+    }
+
+    fn transform_ashr(self, ashr: expr::ArithmeticShiftRight) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(ashr)
+    }
+
+    fn transform_lshr(self, lshr: expr::LogicalShiftRight) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(lshr)
+    }
+
+    fn transform_shl(self, shl: expr::ShiftLeft) -> AnyExprAndTransformResult {
+        AnyExprAndTransformResult::identity(shl)
+    }
 }
 
 /// Simple transformer that does nothing.
@@ -143,6 +283,43 @@ impl<T> AnyTransformer for T where T: Transformer + AutoImplAnyTransformer {
             Not(expr) => self.transform_not(expr),
             And(expr) => self.transform_and(expr),
             Or(expr) => self.transform_or(expr),
+
+            Xor(expr) => self.transform_xor(expr),
+            Implies(expr) => self.transform_implies(expr),
+            ArrayEquals(expr) => self.transform_array_equals(expr),
+            ArrayRead(expr) => self.transform_array_read(expr),
+            ArrayWrite(expr) => self.transform_array_write(expr),
+            Add(expr) => self.transform_add(expr),
+            BitvecConst(expr) => self.transform_bitvec_const(expr),
+            Mul(expr) => self.transform_mul(expr),
+            Neg(expr) => self.transform_neg(expr),
+            SignedDiv(expr) => self.transform_sdiv(expr),
+            SignedModulo(expr) => self.transform_smod(expr),
+            SignedRemainder(expr) => self.transform_srem(expr),
+            Sub(expr) => self.transform_sub(expr),
+            UnsignedDiv(expr) => self.transform_udiv(expr),
+            UnsignedRemainder(expr) => self.transform_urem(expr),
+            BitAnd(expr) => self.transform_bitand(expr),
+            BitNot(expr) => self.transform_bitnot(expr),
+            BitOr(expr) => self.transform_bitor(expr),
+            BitXor(expr) => self.transform_bitxor(expr),
+            Concat(expr) => self.transform_concat(expr),
+            Extract(expr) => self.transform_extract(expr),
+            SignExtend(expr) => self.transform_sext(expr),
+            ZeroExtend(expr) => self.transform_zext(expr),
+            BitvecEquals(expr) => self.transform_bitvec_equals(expr),
+            SignedGreaterEquals(expr) => self.transform_sge(expr),
+            SignedGreaterThan(expr) => self.transform_sgt(expr),
+            SignedLessEquals(expr) => self.transform_sle(expr),
+            SignedLessThan(expr) => self.transform_slt(expr),
+            UnsignedGreaterEquals(expr) => self.transform_uge(expr),
+            UnsignedGreaterThan(expr) => self.transform_ugt(expr),
+            UnsignedLessEquals(expr) => self.transform_ule(expr),
+            UnsignedLessThan(expr) => self.transform_ult(expr),
+            ArithmeticShiftRight(expr) => self.transform_ashr(expr),
+            LogicalShiftRight(expr) => self.transform_lshr(expr),
+            ShiftLeft(expr) => self.transform_shl(expr)
+
             _ => unimplemented!()
         }
     }
