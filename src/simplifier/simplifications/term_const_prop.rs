f8237b9f543d0766cae9cbbe97a693c8363cc223
Hero <robbepop@web.de>
2018-03-01 21:43:54 +0100

    feat(Simplifier): add partial const-evaluation to mul for Term Const Propagator

--- src/simplifier/simplifications/term_const_prop.rs
+++ src/simplifier/simplifications/term_const_prop.rs
@@ -72,6 +72,36 @@ impl Transformer for TermConstPropagator {
                 _ => ()
             }
         }
+
+
+        // If there exist at least two constant child expressions within this and expression
+        // we can evaluate their product and replace the constant child expressions with it.
+        if mul.childs().filter(|c| c.get_if_bitvec_const().is_some()).count() >= 2 {
+            // Split const and non-const child expressions.
+            let (consts, mut rest): (Vec<_>, Vec<_>) = mul.into_childs().partition_map(|c| {
+                match c {
+                    AnyExpr::BitvecConst(c) => Either::Left(c.val),
+                    other                   => Either::Right(other)
+                }
+            });
+            assert!(consts.len() > 0);
+            use itertools::Itertools;
+            // Evalute the product of all constant expressions.
+            let product = consts.into_iter().fold1(|mut lhs, rhs| { lhs *= &rhs; lhs }).unwrap();
+            // If the rest is empty and thus the sum is the only child expression remaining
+            // we can replace the entire and with the sum. Otherwise we just reconstruct the
+            // and expression with the new sum.
+            let result = if rest.is_empty() {
+                AnyExpr::from(expr::BitvecConst::from(product))
+            }
+            else {
+                rest.push(AnyExpr::from(expr::BitvecConst::from(product)));
+                AnyExpr::from(expr::Mul::nary(rest).unwrap())
+            };
+            return TransformOutcome::transformed(result)
+        }
+
+
         TransformOutcome::identity(mul)
     }
 }
@@ -192,6 +222,19 @@ mod tests {
         }
 
         #[test]
+        fn identify_zero_with_const() {
+            let b = PlainExprTreeBuilder::default();
+            let mut expr = b.bitvec_mul_n(vec![
+                b.bitvec_const(BitvecTy::w32(), 42),
+                b.bitvec_var(BitvecTy::w32(), "x"),
+                b.bitvec_const(BitvecTy::w32(), 0)
+            ]).unwrap();
+            simplify(&mut expr);
+            let expected = b.bitvec_const(BitvecTy::w32(), 0).unwrap();
+            assert_eq!(expr, expected);
+        }
+
+        #[test]
         fn binary_with_one() {
             let b = PlainExprTreeBuilder::default();
             let mut expr = b.bitvec_mul_n(vec![
@@ -217,6 +260,53 @@ mod tests {
                 b.bitvec_var(BitvecTy::w32(), "x"),
                 b.bitvec_var(BitvecTy::w32(), "y"),
             ).unwrap();
+            assert_eq!(expr, expected);
+        }
+
+        #[test]
+        fn all_const() {
+            let b = PlainExprTreeBuilder::default();
+            let mut expr = b.bitvec_mul_n(vec![
+                b.bitvec_const(BitvecTy::w32(), 5),
+                b.bitvec_const(BitvecTy::w32(), 7),
+                b.bitvec_const(BitvecTy::w32(), 3)
+            ]).unwrap();
+            simplify(&mut expr);
+            let expected = b.bitvec_const(BitvecTy::w32(), 105).unwrap();
+            assert_eq!(expr, expected);
+        }
+
+        #[test]
+        fn some_const() {
+            let b = PlainExprTreeBuilder::default();
+            let mut expr = b.bitvec_mul_n(vec![
+                b.bitvec_const(BitvecTy::w32(), 11),
+                b.bitvec_var(BitvecTy::w32(), "x"),
+                b.bitvec_const(BitvecTy::w32(), 7)
+            ]).unwrap();
+            simplify(&mut expr);
+            let expected = b.bitvec_mul(
+                b.bitvec_var(BitvecTy::w32(), "x"),
+                b.bitvec_const(BitvecTy::w32(), 77) // swapped since pushed back
+            ).unwrap();
+            assert_eq!(expr, expected);
+        }
+
+        #[test]
+        fn some_const_with_one() {
+            let b = PlainExprTreeBuilder::default();
+            let mut expr = b.bitvec_mul_n(vec![
+                b.bitvec_var(BitvecTy::w32(), "x"),
+                b.bitvec_const(BitvecTy::w32(), 1),
+                b.bitvec_var(BitvecTy::w32(), "y"),
+                b.bitvec_const(BitvecTy::w32(), 42)
+            ]).unwrap();
+            simplify(&mut expr);
+            let expected = b.bitvec_mul_n(vec![
+                b.bitvec_var(BitvecTy::w32(), "x"),
+                b.bitvec_var(BitvecTy::w32(), "y"),
+                b.bitvec_const(BitvecTy::w32(), 42)
+            ]).unwrap();
             assert_eq!(expr, expected);
         }
     }
