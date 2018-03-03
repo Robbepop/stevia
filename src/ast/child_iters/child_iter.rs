843429803ff326435889c42cb5bf15ca85d9c0e1
Hero <robbepop@web.de>
2018-03-03 12:10:45 +0100

    feat(AST): add test suites for childs, childs_mut and into_childs iterators

--- src/ast/child_iters/child_iter.rs
+++ src/ast/child_iters/child_iter.rs
@@ -143,3 +143,125 @@ impl<'p> DoubleEndedIterator for ChildsIter<'p> {
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn none() {
+		let bool_const = expr::BoolConst::t();
+		let mut iter = bool_const.childs();
+		assert_eq!(iter.next(), None)
+	}
+
+	#[test]
+	fn unary() {
+		let b = PlainExprTreeBuilder::default();
+		let expr = b.not(b.bool_const(false)).unwrap();
+		let mut iter = expr.childs();
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next(), None);
+	}
+
+	#[test]
+	fn binary() {
+		let b = PlainExprTreeBuilder::default();
+		let expr = b.and(
+			b.bool_const(false),
+			b.bool_const(true)
+		).unwrap();
+		let mut iter = expr.childs();
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BoolConst::t())));
+		assert_eq!(iter.next(), None);
+	}
+
+	fn test_cond() -> AnyExpr {
+		let b = PlainExprTreeBuilder::default();
+		b.cond(
+			b.bool_const(false),
+			b.bitvec_const(BitvecTy::w32(), 42),
+			b.bitvec_const(BitvecTy::w32(), 5),
+		).unwrap()
+	}
+
+	#[test]
+	fn ternary() {
+		let expr = test_cond();
+		let mut iter = expr.childs();
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next(), None);
+	}
+
+	fn big_test_expr() -> AnyExpr {
+		let b = PlainExprTreeBuilder::default();
+		b.bitvec_add_n(vec![
+			b.bitvec_const(BitvecTy::w32(), 42),
+			b.bitvec_const(BitvecTy::w32(), 1337),
+			b.bitvec_const(BitvecTy::w32(), 77),
+			b.bitvec_const(BitvecTy::w32(), 0),
+			b.bitvec_const(BitvecTy::w32(), 5),
+		]).unwrap()
+	}
+
+	#[test]
+	fn nary() {
+		let expr = big_test_expr();
+		let mut iter = expr.childs();
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(1337))));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(77))));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(0))));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next(), None);
+	}
+
+	#[test]
+	fn ternary_next_back() {
+		let expr = test_cond();
+		let mut iter = expr.childs();
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next_back(), None);
+	}
+
+	#[test]
+	fn nary_next_back() {
+		let expr = big_test_expr();
+		let mut iter = expr.childs();
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(0))));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(77))));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(1337))));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next_back(), None);
+	}
+
+	#[test]
+	fn ternary_next_and_next_back() {
+		let expr = test_cond();
+		let mut iter = expr.childs();
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next(), None);
+		assert_eq!(iter.next_back(), None);
+	}
+
+	#[test]
+	fn nary_next_and_next_back() {
+		let expr = big_test_expr();
+		let mut iter = expr.childs();
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(0))));
+		assert_eq!(iter.next(), Some(&AnyExpr::from(expr::BitvecConst::from(1337))));
+		assert_eq!(iter.next_back(), Some(&AnyExpr::from(expr::BitvecConst::from(77))));
+		assert_eq!(iter.next(), None);
+		assert_eq!(iter.next_back(), None);
+	}
+}
--- src/ast/child_iters/child_iter_mut.rs
+++ src/ast/child_iters/child_iter_mut.rs
@@ -139,3 +139,125 @@ impl<'p> DoubleEndedIterator for ChildsIterMut<'p> {
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn none() {
+		let mut bool_const = expr::BoolConst::t();
+		let mut iter = bool_const.childs_mut();
+		assert_eq!(iter.next(), None)
+	}
+
+	#[test]
+	fn unary() {
+		let b = PlainExprTreeBuilder::default();
+		let mut expr = b.not(b.bool_const(false)).unwrap();
+		let mut iter = expr.childs_mut();
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next(), None);
+	}
+
+	#[test]
+	fn binary() {
+		let b = PlainExprTreeBuilder::default();
+		let mut expr = b.and(
+			b.bool_const(false),
+			b.bool_const(true)
+		).unwrap();
+		let mut iter = expr.childs_mut();
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BoolConst::t())));
+		assert_eq!(iter.next(), None);
+	}
+
+	fn test_cond() -> AnyExpr {
+		let b = PlainExprTreeBuilder::default();
+		b.cond(
+			b.bool_const(false),
+			b.bitvec_const(BitvecTy::w32(), 42),
+			b.bitvec_const(BitvecTy::w32(), 5),
+		).unwrap()
+	}
+
+	#[test]
+	fn ternary() {
+		let mut expr = test_cond();
+		let mut iter = expr.childs_mut();
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next(), None);
+	}
+
+	fn big_test_expr() -> AnyExpr {
+		let b = PlainExprTreeBuilder::default();
+		b.bitvec_add_n(vec![
+			b.bitvec_const(BitvecTy::w32(), 42),
+			b.bitvec_const(BitvecTy::w32(), 1337),
+			b.bitvec_const(BitvecTy::w32(), 77),
+			b.bitvec_const(BitvecTy::w32(), 0),
+			b.bitvec_const(BitvecTy::w32(), 5),
+		]).unwrap()
+	}
+
+	#[test]
+	fn nary() {
+		let mut expr = big_test_expr();
+		let mut iter = expr.childs_mut();
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(1337))));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(77))));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(0))));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next(), None);
+	}
+
+	#[test]
+	fn ternary_next_back() {
+		let mut expr = test_cond();
+		let mut iter = expr.childs_mut();
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next_back(), None);
+	}
+
+	#[test]
+	fn nary_next_back() {
+		let mut expr = big_test_expr();
+		let mut iter = expr.childs_mut();
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(0))));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(77))));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(1337))));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next_back(), None);
+	}
+
+	#[test]
+	fn ternary_next_and_next_back() {
+		let mut expr = test_cond();
+		let mut iter = expr.childs_mut();
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BoolConst::f())));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next(), None);
+		assert_eq!(iter.next_back(), None);
+	}
+
+	#[test]
+	fn nary_next_and_next_back() {
+		let mut expr = big_test_expr();
+		let mut iter = expr.childs_mut();
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(5))));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(42))));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(0))));
+		assert_eq!(iter.next(), Some(&mut AnyExpr::from(expr::BitvecConst::from(1337))));
+		assert_eq!(iter.next_back(), Some(&mut AnyExpr::from(expr::BitvecConst::from(77))));
+		assert_eq!(iter.next(), None);
+		assert_eq!(iter.next_back(), None);
+	}
+}
