ac1ef2301c423d880de3bb95e1d5dcde2fa33a8f
Hero <robbepop@web.de>
2018-03-10 10:07:22 +0100

    feat(Simplifier): add initial draft of the TermSymbSolver (merge multiplicities)

--- src/simplifier/simplifications/term_symb_solver.rs
+++ src/simplifier/simplifications/term_symb_solver.rs
@@ -1,6 +1,10 @@
 use ast::prelude::*;
 
+use apint::ApInt;
+use itertools::Itertools;
 
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use either::Either;
 
 pub mod prelude {
@@ -16,19 +20,106 @@ pub struct TermSymbolicSolver;
 
 impl AutoImplAnyTransformer for TermSymbolicSolver {}
 
+fn has_like_terms(add: &expr::Add) -> bool {
+    let width = add.bitvec_ty.width();
+    let mut seen_symbols: HashMap<&AnyExpr, ApInt> = HashMap::new();
+    let mut mutated = false;
+    let (negs, rest): (Vec<_>, Vec<_>) = add.childs().partition_map(|c| {
+        match c {
+            AnyExpr::Neg(neg) => Either::Left(neg),
+            other             => Either::Right(other)
+        }
+    });
+    let (muls, rest): (Vec<_>, Vec<_>) = rest.into_iter().partition_map(|c| {
+        match c {
+            AnyExpr::Mul(mul) => Either::Left(mul),
+            other             => Either::Right(other)
+        }
+    });
+    for child in rest {
+        match seen_symbols.entry(child) {
+            Entry::Occupied(mut occupied) => {
+                occupied.get_mut()
+                        .checked_add_assign(
+                            &ApInt::one(width.raw_width())).unwrap();
+                mutated = true;
+            }
+            Entry::Vacant(vacant) => {
+                vacant.insert(ApInt::one(width.raw_width()));
+            }
         }
     }
+    for neg in negs {
+        match seen_symbols.entry(neg.single_child()) {
+            Entry::Occupied(mut occupied) => {
+                occupied.get_mut()
+                        .checked_add_assign(
+                            // `ApInt::all_set` behaves as if it was -1 in twos-complement
+                            &ApInt::all_set(width.raw_width())).unwrap();
+                mutated = true;
+            }
+            Entry::Vacant(vacant) => {
+                vacant.insert(ApInt::all_set(width.raw_width()));
+            }
+        }
+    }
+    for mul in muls {
+        if mul.arity() != 2 {
+            continue;
+        }
+        if mul.childs().filter(|c| c.kind() == ExprKind::BitvecConst).count() != 1 {
+            continue;
+        }
+        let mut mul_childs = mul.childs();
+        let lhs = mul_childs.next().unwrap();
+        let rhs = mul_childs.next().unwrap();
+        match (lhs, rhs) {
+            (AnyExpr::BitvecConst(lhs), rhs) => {
+                match seen_symbols.entry(rhs) {
+                    Entry::Occupied(mut occupied) => {
+                        occupied.get_mut()
+                                .checked_add_assign(&lhs.val).unwrap();
+                        mutated = true;
+                    }
+                    Entry::Vacant(vacant) => {
+                        vacant.insert(lhs.val.clone());
                     }
                 }
             }
+            (lhs, AnyExpr::BitvecConst(rhs)) => {
+                match seen_symbols.entry(lhs) {
+                    Entry::Occupied(mut occupied) => {
+                        occupied.get_mut()
+                                .checked_add_assign(&rhs.val).unwrap();
+                        mutated = true;
+                    }
+                    Entry::Vacant(vacant) => {
+                        vacant.insert(rhs.val.clone());
+                    }
+                }
             }
+            _ => ()
         }
+    }
+    return mutated;
 }
 
+fn simplify_add(add: expr::Add) -> TransformOutcome {
+    if has_like_terms(&add) {
+        // let (mut eqs, mut rest): (Vec<_>, Vec<_>) = and.into_childs().partition_map(|c| {
+        //     match c {
+        //         AnyExpr::$eq_type(eq) => Either::Left(eq),
+        //         other                 => Either::Right(other)
+        //     }
+        // });
+        unimplemented!()
     }
+    TransformOutcome::identity(add)
 }
 
 impl Transformer for TermSymbolicSolver {
+    fn transform_add(&self, add: expr::Add) -> TransformOutcome {
+        simplify_add(add)
     }
 }
 
@@ -65,8 +156,34 @@ mod tests {
         PlainExprTreeBuilder::default()
     }
 
+    #[test]
+    fn complex() {
+        let b = new_builder();
+        assert_simplified(
+            b.bitvec_add_n(vec![
+                b.bitvec_var(BitvecTy::w32(), "x"),
+                b.bitvec_neg(b.bitvec_var(BitvecTy::w32(), "x")),
+                b.bitvec_mul(
+                    b.bitvec_var(BitvecTy::w32(), "y"),
+                    b.bitvec_const(BitvecTy::w32(), -3_i32)
                 ),
+                b.bitvec_var(BitvecTy::w32(), "y"),
+                b.bitvec_mul(
+                    b.bitvec_var(BitvecTy::w32(), "x"),
+                    b.bitvec_const(BitvecTy::w32(), 5_i32)
+                ),
+                b.bitvec_neg(b.bitvec_var(BitvecTy::w32(), "x"))
+            ]),
+            b.bitvec_add(
+                b.bitvec_mul(
+                    b.bitvec_var(BitvecTy::w32(), "x"),
+                    b.bitvec_const(BitvecTy::w32(), 4_i32)
+                ),
+                b.bitvec_mul(
+                    b.bitvec_var(BitvecTy::w32(), "y"),
+                    b.bitvec_const(BitvecTy::w32(), -2_i32)
                 ),
             )
+        )
     }
 }
