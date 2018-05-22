d5d39f1b92ab5bcadb79092497c930484078e7f9
Hero Bird <robbepop@web.de>
2018-05-22 22:17:50 +0200

    refactor(Bitblaster): base bitblaster off the new gate encoder

--- stevia_bitblaster/src/bitblaster.rs
+++ stevia_bitblaster/src/bitblaster.rs
@@ -4,53 +4,56 @@
 //! and should also make it quite easy to convert into AIG if needed.
 //! Also they allow for a higher-level bit blaster implementation
 //! than when directly constructing CNF or AIG.
-//! 
+//!
 //! As with CNF this data structure allows for a streaming bit blaster
 //! implementation that does not need to allocate temporary data structures
 //! for partial construction.
-//! 
+//!
 //! This has the effect that after streaming conversion to CNF one could
 //! simply and directly forward this to an IPASIR implementing SAT
 //! solver in an efficient way.
 
-use repr::{VarPack};
-use bit_encoder::{BitEncoder};
+use gate_encoder::{GateEncoder, LitGen, Output, RawGateEncoder};
+use repr::LitPack;
 
-pub fn bitblast_add<C>(enc: C, lhs: VarPack, rhs: VarPack) -> Result<VarPack, String>
+pub fn bitblast_add<G, E>(
+    enc: GateEncoder<G, E>,
+    lhs: LitPack,
+    rhs: LitPack,
+) -> Result<LitPack, String>
 where
-    C: BitEncoder
+    G: LitGen,
+    E: RawGateEncoder,
 {
     if lhs.len() != rhs.len() {
         return Err(String::from(
-            "bitblast_add: error: left hand-side and right hand-side have different bit widths"
-        ))
+            "bitblast_add: error: left hand-side and right hand-side have different bit widths",
+        ));
     }
     let width = lhs.len();
     // Allocate result and carry bits
-    let res = enc.new_var_pack(width);
-    let carries = enc.new_var_pack(width);
+    let res = enc.new_lit_pack(width);
+    let carries = enc.new_lit_pack(width);
     // Compute least-significant bit
-    enc.def(res.i(0), enc.xor(lhs.i(0), rhs.i(0)));
-    enc.def(carries.i(0), enc.or(&[lhs.i(0), rhs.i(0)]));
+    enc.xor_with_output(lhs.i(0), rhs.i(0), Output(res.i(0)));
+    enc.or_with_output(&[lhs.i(0), rhs.i(0)], Output(carries.i(0)));
     // Compute result for all other bits
     for i in 1..width {
         // Calculation of result_i
-        enc.def(
-            res.i(i),
-            enc.xor(
-                enc.xor(lhs.i(i), rhs.i(i)),
-                carries.i(i-1)
-            )
+        enc.xor_with_output(
+            enc.xor(lhs.i(i), rhs.i(i)),
+            carries.i(i - 1),
+            Output(res.i(i)),
         );
         // Calculation of carry_i
-        enc.def(
-            carries.i(i),
-            enc.and(&[
+        enc.and_with_output(
+            &[
                 enc.or(&[lhs.i(i), rhs.i(i)]),
-                enc.or(&[lhs.i(i), carries.i(i-1)]),
-                enc.or(&[rhs.i(i), carries.i(i-1)])
-            ])
-        );
+                enc.or(&[lhs.i(i), carries.i(i - 1)]),
+                enc.or(&[rhs.i(i), carries.i(i - 1)]),
+            ],
+            Output(carries.i(i)),
+        )
     }
     Ok(res)
 }
