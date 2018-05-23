5458bf9280b304e9edfd621158bf0e56273b052a
Hero Bird <robbepop@web.de>
2018-05-23 14:20:43 +0200

    refactor(Bitblaster): add Bitblaster struct to hold state during bitblasting

--- stevia_bitblaster/src/bitblaster.rs
+++ stevia_bitblaster/src/bitblaster.rs
@@ -16,11 +16,8 @@
 use gate_encoder::{GateEncoder, LitGen, Output, RawGateEncoder};
 use repr::LitPack;
 
-pub fn bitblast_add<G, E>(
-    enc: GateEncoder<G, E>,
-    lhs: LitPack,
-    rhs: LitPack,
-) -> Result<LitPack, String>
+type Result<T> = ::std::result::Result<T, String>;
+
 mod checks {
     use super::Result;
     use repr::LitPack;
@@ -37,39 +34,47 @@ mod checks {
     }
 }
 
+struct Bitblaster<G, E>
 where
     G: LitGen,
-    E: RawGateEncoder,
+    E: RawGateEncoder
 {
-    if lhs.len() != rhs.len() {
-        return Err(String::from(
-            "bitblast_add: error: left hand-side and right hand-side have different bit widths",
-        ));
+    enc: GateEncoder<G, E>
+}
+
+impl<G, E> Bitblaster<G, E>
+where
+    G: LitGen,
+    E: RawGateEncoder
+{
     }
-    let width = lhs.len();
-    // Allocate result and carry bits
-    let res = enc.new_lit_pack(width);
-    let carries = enc.new_lit_pack(width);
-    // Compute least-significant bit
-    enc.xor_with_output(lhs(0), rhs(0), Output(res(0)));
-    enc.or_with_output(&[lhs(0), rhs(0)], Output(carries(0)));
-    // Compute result for all other bits
-    for i in 1..width {
-        // Calculation of result_i
-        enc.xor_with_output(
-            enc.xor(lhs(i), rhs(i)),
-            carries(i - 1),
-            Output(res(i)),
-        );
-        // Calculation of carry_i
-        enc.and_with_output(
-            &[
-                enc.or(&[lhs(i), rhs(i)]),
-                enc.or(&[lhs(i), carries(i - 1)]),
-                enc.or(&[rhs(i), carries(i - 1)]),
-            ],
-            Output(carries(i)),
-        )
+
+    fn bitblast_add(&self, lhs: LitPack, rhs: LitPack) -> Result<LitPack> {
+        let width = checks::assert_litpack_len(lhs, rhs)?;
+        // Allocate result and carry bits
+        let res = self.enc.new_lit_pack(width);
+        let carries = self.enc.new_lit_pack(width);
+        // Compute least-significant bit
+        self.enc.xor_with_output(lhs(0), rhs(0), Output(res(0)));
+        self.enc.or_with_output(&[lhs(0), rhs(0)], Output(carries(0)));
+        // Compute result for all other bits
+        for i in 1..width {
+            // Calculation of result_i
+            self.enc.xor_with_output(
+                self.enc.xor(lhs(i), rhs(i)),
+                carries(i - 1),
+                Output(res(i)),
+            );
+            // Calculation of carry_i
+            self.enc.and_with_output(
+                &[
+                    self.enc.or(&[lhs(i), rhs(i)]),
+                    self.enc.or(&[lhs(i), carries(i - 1)]),
+                    self.enc.or(&[rhs(i), carries(i - 1)]),
+                ],
+                Output(carries(i)),
+            )
+        }
+        Ok(res)
     }
-    Ok(res)
 }
