c33ad00d81c886814c3191bb9a20373604eab7b7
Hero Bird <robbepop@web.de>
2018-06-07 00:16:35 +0200

    feat(Lexer): add LexerError and utils around it

--- stevia_parser/src/lib.rs
+++ stevia_parser/src/lib.rs
@@ -78,6 +78,9 @@
 #[macro_use]
 extern crate indoc;
 
+use std::error;
+use std::fmt;
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Loc(u32);
 
@@ -176,6 +179,93 @@ impl CharAndLoc {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum LexerErrorKind {
+    UnexpectedEndOfFile,
+    UnexpectedCharacter(char),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct LexerError {
+    kind: LexerErrorKind,
+    span: Span,
+    context: Option<String>,
+}
+
+impl LexerError {
+    fn new(kind: LexerErrorKind, span: Span) -> Self {
+        Self {
+            kind,
+            span,
+            context: None,
+        }
+    }
+
+    pub fn context_msg<S>(self, msg: S) -> Self
+    where
+        S: Into<String>,
+    {
+        let mut this = self;
+        this.context = Some(msg.into());
+        this
+    }
+
+    pub fn kind(&self) -> LexerErrorKind {
+        self.kind
+    }
+
+    pub fn span(&self) -> Span {
+        self.span
+    }
+
+    pub fn context(&self) -> &str {
+        match &self.context {
+            None => "",
+            Some(s) => s
+        }
+    }
+
+    pub fn unexpected_end_of_file(span: Span) -> Self {
+        Self::new(LexerErrorKind::UnexpectedEndOfFile, span)
+    }
+
+    pub fn unexpected_character(span: Span, ch: char) -> Self {
+        Self::new(LexerErrorKind::UnexpectedCharacter(ch), span)
+    }
+}
+
+impl fmt::Display for LexerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::LexerErrorKind::*;
+        match self.kind() {
+            UnexpectedEndOfFile => write!(
+                f,
+                "error ({}:{}): unexpected end of file: {:?}",
+                self.span.begin.to_u32(),
+                self.span.end.to_u32(),
+                self.context
+            ),
+            UnexpectedCharacter(ch) => write!(
+                f,
+                "error ({}:{}): unexpected character (= {}): {:?}",
+                self.span.begin.to_u32(),
+                self.span.end.to_u32(),
+                ch,
+                self.context
+            )
+        }
+    }
+}
+
+impl error::Error for LexerError {
+    fn description(&self) -> &str {
+        use self::LexerErrorKind::*;
+        match self.kind() {
+            UnexpectedEndOfFile => "unexpected end of file",
+            UnexpectedCharacter(_) => "unexpected character",
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct LexemeIter<'c> {
