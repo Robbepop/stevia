32291bd1d9cee136862ba87f3d44e7b1f648d9cd
Hero Bird <robbepop@web.de>
2018-06-16 03:18:25 +0200

    refactor(Parser): initial public module interface for the parser

--- stevia_parser/src/lib.rs
+++ stevia_parser/src/lib.rs
@@ -7,3 +7,37 @@ extern crate lazy_static;
 mod lexer;
 mod parser;
 mod commands;
+
+pub use self::{
+    lexer::{
+        LexerError,
+        LexerErrorKind,
+        smtlib2_tokens,
+        TokenIter,
+        Loc,
+        Span,
+        Command
+    },
+    parser::{
+        parse_smtlib2,
+        Parser,
+        PropLitsIter,
+        ParseResult,
+        ParseErrorKind,
+        ParseError
+    },
+    commands::{
+        ResponseError,
+        ResponseResult,
+        OptionKindBase,
+        OptionKind,
+        LiteralBase,
+        Literal,
+        NumeralLitBase,
+        OutputChannelBase,
+        OutputChannel,
+        OptionAndValueBase,
+        OptionAndValue,
+        SMTLib2Solver
+    }
+};
